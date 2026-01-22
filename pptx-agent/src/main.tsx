13c4e29f0ff11e625e78ca25f7322d21679fd58d
minorun365 <minorun365@users.noreply.github.com>
2026-01-22 22:10:11 +0900

    更新

--- pptx-agent/src/main.tsx
+++ pptx-agent/src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Amplify } from 'aws-amplify'
+import { I18n } from 'aws-amplify/utils'
+import { translations } from '@aws-amplify/ui-react'
 import outputs from '../amplify_outputs.json'
 import './index.css'
 import App from './App.tsx'
 
 Amplify.configure(outputs)
+I18n.putVocabularies(translations)
+I18n.setLanguage('ja')
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
